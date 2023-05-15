e2d5a4e0d51a511f0c4d4c66f129e89bf31cde8f
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-05-15 12:06:19 +0400

    adding functionality to the burger bar

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+let burgerEl = document.querySelector('.burger');
+let navigationEl = document.querySelector('.navigation');
+let navIconEl = document.querySelector('.nav-icon');
+let headerPEl =document.querySelector('.header-paragraph');
+let underlineEl = document.querySelector('.underline');
+let navigationContainerEl = document.querySelector('.navigation-container');
+let topElement = document.querySelector('.top')
+let middleElement = document.querySelector('.middle')
+let bottomElement = document.querySelector('.bottom')
+
+
+burgerEl.addEventListener('click', function(){
+    navigationEl.classList.toggle('enable');
+    navIconEl.classList.toggle('disable');
+    headerPEl.classList.toggle('disable');
+    underlineEl.classList.toggle('disable');
+    navigationContainerEl.classList.toggle('set-height')
+    topElement.classList.toggle('displayTop')
+    middleElement.classList.toggle('disable')
+    bottomElement.classList.toggle('displayBottom')
+})
