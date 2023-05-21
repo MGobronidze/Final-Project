b4bca74060118f9087a62d3c043064b4c8cee253
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-05-21 13:19:09 +0400

    add subscribe validation

--- javascript/subscribe.js
+++ javascript/subscribe.js
@@ -1,2 +1,19 @@
 // subscribe form validation 
+let subsEl =document.getElementById('subscribe-form');
 
+subsEl.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let subsEmailField = document.getElementById('subs-email');
+    let subsEmailValue = subsEmailField.value;
+    let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    
+    if(subsEmailValue.match(emailRegex)){
+        subsEmailField.style.border ='2px solid green'; 
+    } else{
+        subsEmailField.style.border ='2px solid red'; 
+        alert('Please, Enter valid Email Adress!')  
+        return
+    }
+
+})
